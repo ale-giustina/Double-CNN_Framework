a082753c7c7fe5a50ab989963358afa0bfae4fdd
alexjust67 <alexjusti67n@gmail.com>
2024-04-04 00:00:55 +0200

    Training phase, todo the dataset must still be normalized

--- Trainer/module1.py
+++ Trainer/module1.py
@@ -2,13 +2,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-import pickle
 from torch.utils.data import Dataset, DataLoader
 from torchvision.io import read_image
 import math
 import time
 import os
-from scipy import ndimage
+import matplotlib.pyplot as plt
 
 class CustomImageDataset(Dataset):
     def __init__(self, annotations_dir, img_dir, transform=None, target_transform=None):
@@ -23,167 +22,96 @@ class CustomImageDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.imlist[idx])
-        image = read_image(img_path)
+        image = read_image(img_path).type(torch.float)
         label_path = os.path.join(self.labels_dir, "ann_"+self.imlist[idx])
-        label = read_image(label_path)
+        label = read_image(label_path).type(torch.float)
 
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
+
+        return image, label #convert to float from unit8
+#TODO: NORMALIZE THE DATAAAAAAAAAAAAAAAAA
+
+class CNNet(nn.Module):
+    def __init__(self):
+        super(CNNet, self).__init__()
+                
+        self.conv1 = nn.Conv2d(3, 25, kernel_size=9, stride=1, padding=4)
+        self.relu1 = nn.LeakyReLU(inplace=True)
+        self.conv2 = nn.Conv2d(25, 15, kernel_size=5, stride=1, padding=2)
+        self.relu2 = nn.LeakyReLU(inplace=True)
+        self.conv3 = nn.Conv2d(15, 1, kernel_size=1, stride=1)
+        self.relu3 = nn.LeakyReLU(inplace=True)
         
-        return image, label
+        self.steps = 0
+        self.epochs = 0
+        self.best_valdiation_loss = math.inf
 
-class net(nn.Module):
-	def __init__(self,input_size,output_size):
-		super(net,self).__init__()
-		
-		self.relu = nn.LeakyReLU()
-		self.l1 = nn.Linear(input_size,126)
-		self.l2 = nn.Linear(126,126)
-		self.l3 = nn.Linear(126,50)
-		self.l4 = nn.Linear(50,output_size)
-		self.sftmx = nn.Softmax(dim=1)
-
-		self.steps = 0
-		self.epochs = 0
-		self.best_valdiation_loss = math.inf
-
-	def forward(self,x):
-		
-		output = self.l1(x) 
-		output = self.relu(output)
-		output = self.l2(output)
-		output = self.relu(output)
-		output = self.l3(output)
-		output = self.relu(output)
-		output = self.l4(output)
-		print(output.shape)
-		if self.training: output = self.sftmx(output)
-
-		if self.training:
-			self.steps += 1
-		
-		return output
-
-class UNet(nn.Module):
-	def __init__(self):
-		super(UNet, self).__init__()
-		
-		# Encoder
-		self.encoder_conv1 = nn.Conv2d(3, 64, 3, padding=1)
-		self.encoder_conv2 = nn.Conv2d(64, 128, 3, padding=1)
-		self.encoder_pool = nn.MaxPool2d(2, 2)
-		self.steps = 0
-		self.epochs = 0
-		self.best_valdiation_loss = math.inf
-		# Decoder
-		self.decoder_upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-		self.decoder_conv1 = nn.Conv2d(256, 64, 3, padding=1)
-		self.decoder_conv2 = nn.Conv2d(64, 64, 3, padding=1)
-		self.decoder_conv3 = nn.Conv2d(64, 1, 1)  # Output density map with 1 channel
-		
-	def forward(self, x):
-		# Encoder
-		x1 = nn.functional.relu(self.encoder_conv1(x))
-		x2 = nn.functional.relu(self.encoder_conv2(x1))
-		x3 = self.encoder_pool(x2)
-		
-		# Decoder
-		x4 = self.decoder_upsample(x3)
-		x4 = torch.cat((x4, x2), dim=1)
-		x5 = nn.functional.relu(self.decoder_conv1(x4))
-		x5 = nn.functional.relu(self.decoder_conv2(x5))
-		x6 = self.decoder_conv3(x5)
-		if self.training:
-			self.steps += 1
-		return x6
-
-class DensityMapModel(nn.Module):
-	def __init__(self):
-		super(DensityMapModel, self).__init__()
-				
-		# Define the layers of your model
-		self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
-		self.relu1 = nn.LeakyReLU(inplace=True)
-		self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-		self.relu2 = nn.LeakyReLU(inplace=True)
-		self.conv3 = nn.Conv2d(64, 1, kernel_size=1, stride=1)
-		self.relu3 = nn.LeakyReLU(inplace=True)
-		self.steps = 0
-		self.epochs = 0
-		self.best_valdiation_loss = math.inf
-	def forward(self, x):
-		x = self.conv1(x)
-		x = self.relu1(x)
-		x = self.conv2(x)
-		x = self.relu2(x)
-		x = self.conv3(x)
-		density_map = self.relu3(x)
-				
-		if self.training:
-			self.steps += 1
-				
-		return torch.squeeze(density_map)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.conv3(x)
+        density_map = self.relu3(x)
+                
+        if self.training:
+            self.steps += 1
+                
+        return density_map
 
 
-def clustercount2(density_map, treshold, mxlen=17):
-	density_map = (density_map >= treshold) * density_map
-	density_map = ndimage.measurements.label(density_map)[0]
-	maxval = np.max(density_map)
-	return maxval
 
 
-def calculate_loss_and_accuracy(validation_loader, model, criterion, stop_at = 1200, print_every=99999):
-	correct = 0
-	total = 0
-	steps = 0
-	total_loss = 0
-	sz = len(validation_loader)
-	delta=[]
-	for images, labels in validation_loader:
-	
-		if total%print_every == 0 and total > 0:
-			accuracy = 100 * correct / total
-			print(accuracy)
-		
-		if total >= stop_at:
-			break;
-		if torch.cuda.is_available():
-			images = images.cuda()
-			labels = labels.cuda()
+def calculate_loss_and_accuracy(validation_loader, model, criterion,example_image,epoch):
+    
+    total = 0
+    steps = 0
+    total_loss = 0
+    sz = len(validation_loader)
+    delta=[]
+    
+    for images, labels in validation_loader:
+    
+        if torch.cuda.is_available():
+            images = images.cuda()
+            labels = labels.cuda()
 
-		# Forward pass only to get logits/output
-		outputs = model(images)
-		
-		#Get Loss for validation data
-		loss = criterion(outputs, labels)
-		total_loss += loss.item()
+        # Forward pass only to get logits/output
+        outputs = model(images)
+        
+        #Get Loss for validation data
+        loss = criterion(outputs, labels)
+        total_loss += loss.item()
 
-		# Get predictions from the maximum value
-		predicted=clustercount2(outputs.to('cpu').detach(),1)
-		labels=clustercount2(labels.to('cpu'),1)
+        #save an image of the output, the input and the label
+        output_example = model(example_image[0].cuda()).cpu().detach().numpy()
+        input_example = example_image[0]
+        label_example = example_image[1]
+        #create a matplotlib plot
+        fig, ax = plt.subplots(1,3)
+        ax[0].imshow(np.transpose(input_example,(1,2,0)).numpy().astype(np.uint8))
+        ax[1].imshow(np.transpose(output_example,(1,2,0)))
+        ax[2].imshow(np.transpose(label_example,(1,2,0)))
+        plt.show()
+        plt.savefig('examples/{}.png'.format(epoch), dpi=800)
+        plt.close(fig)
 
-		delta.append(abs(predicted-labels))
+        # Total number of labels
+        steps += 1
 
-		# Total number of labels
-		total += 1
-		steps += 1
+        del outputs, loss
 
-		correct += (predicted == labels).sum().item()
-
-		del outputs, loss, predicted
-
-	accuracy = 100 * correct / total
-	deltaval=np.mean(np.array(delta))
-	return total_loss/steps, accuracy,deltaval
+    return total_loss/steps
 
 
 def save_model(model, use_ts=False,curr_folder='./mdl'):
-	if not os.path.exists(curr_folder):
-		os.makedirs(curr_folder) 
-	if use_ts:
-		time_stamp = time.strftime("%d_%b_%Y_%Hh%Mm", time.gmtime())
-		torch.save(model, curr_folder + '/{}.ckp'.format(time_stamp))		#model.state_dict()
-	else:
-		torch.save(model, curr_folder + '/{}.ckp'.format('best_model'))		#model.state_dict()
+    if not os.path.exists(curr_folder):
+        os.makedirs(curr_folder) 
+    if use_ts:
+        time_stamp = time.strftime("%d_%b_%Y_%Hh%Mm", time.gmtime())
+        torch.save(model, curr_folder + '/{}.ckp'.format(time_stamp))        #model.state_dict()
+    else:
+        torch.save(model, curr_folder + '/{}.ckp'.format('best_model'))        #model.state_dict()
--- Trainer/trainer.py
+++ Trainer/trainer.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.transforms as T
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import module1 as m1
@@ -15,16 +16,19 @@ writer = SummaryWriter()
 device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
 
-batch_size = 14
-steps_before_print = 50
-step_size = 2
-showexample = 15
+batch_size = 5
+steps_before_print = 20
+showexample = 4
 
-#TODO: create datasets
+transform = T.Compose([T.Resize((1080, 1920))])
 
-dataset = m1.CustomImageDataset("Dataset/annotated", "Dataset/Train")
+transform1 = T.Compose([T.Resize((1080, 1920))])
 
-validataset = m1.CustomImageDataset("Dataset/annotated", "Dataset/Val")
+#TODO: Normalize the data well
+
+dataset = m1.CustomImageDataset("Dataset/annotated", "Dataset/Train",transform=transform, target_transform=transform1)
+
+validataset = m1.CustomImageDataset("Dataset/annotated", "Dataset/Val",transform=transform, target_transform=transform1)
 
 dataloader = DataLoader(dataset=dataset,shuffle=True,batch_size=batch_size)
 
@@ -42,7 +46,7 @@ if showexample != 0:
 
     fig, axs = plt.subplots(1, 2)  # 1 row, 2 columns
 
-    axs[0].imshow(image1)
+    axs[0].imshow(image1.numpy().astype(np.uint8))
     axs[0].set_title('Image 1')
 
     axs[1].imshow(label1)
@@ -51,7 +55,6 @@ if showexample != 0:
     plt.show()
 
 
-#TODO: create model
 model = m1.CNNet()
 
 model.to(device)
@@ -94,27 +97,26 @@ def train(epochs):
 
             step += 1
             epoch_loss += loss.item()
-
+            print(step, loss.item())
 
             if step % steps_before_print == 0:
                 
                 # Calculate Accuracy
                 model.eval()
-                #TODO: calculate accuracy
-                validation_loss, accuracy, deltaval = m1.calculate_loss_and_accuracy(validataloader, model, criterion, stop_at = 1200)
+
+                validation_loss = m1.calculate_loss_and_accuracy(validataloader, model, criterion, dataset.__getitem__(5),epoch)
                 writer.add_scalar('validation_loss', validation_loss, model.steps)
-                writer.add_scalar('accuracy', accuracy, model.steps)
-                writer.add_scalar('deltaval', deltaval, model.steps)
-                epoch_acc += accuracy
+
                 times_calculated += 1
                 
                 # Print Loss
-                print('Iteration: {}/{} - ({:.2f}%). Loss: {}. Accuracy: {}, deltaavg:{}'.format(step, total_size, step*100/total_size , loss.item(), accuracy,deltaval))
+                print('Iteration: {}/{} - ({:.2f}%). Validation Loss: {}. '.format(step, total_size, step*100/total_size , validation_loss))
                 
-                if abs(deltaval) < model.best_valdiation_loss:
-                    model.best_valdiation_loss = deltaval
+                if validation_loss < model.best_valdiation_loss:
+                    model.best_valdiation_loss = validation_loss
                     print('Saving best model')
                     m1.save_model(model)
+
                 del validation_loss
                 
             del loss, outputs, images, labels
@@ -125,12 +127,12 @@ def train(epochs):
         print('Epoch ', epoch)
         #save_model(model, use_ts=True)
 
-learning_rate = 0.01
+learning_rate = 0.00001
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
 train(50)
-learning_rate = 0.001
+learning_rate = 0.0001
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
 train(20)
-learning_rate = 0.0001
+learning_rate = 0.00001
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
 train(20)
